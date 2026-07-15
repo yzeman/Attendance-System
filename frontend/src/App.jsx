ef9b4088c371b4fe8b9547b42ec39fac723a5d74
yzeman <127164094+yzeman@users.noreply.github.com>
2026-07-15 15:00:40 +0100

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import StudentDashboard from './components/student/StudentDashboard';
 import MarkAttendance from './components/student/MarkAttendance';
+import MyProfile from './components/student/MyProfile';
 import AdminDashboard from './components/admin/AdminDashboard';
 import Navbar from './components/common/Navbar';
 import ProtectedRoute from './components/common/ProtectedRoute';
@@ -30,6 +31,14 @@ function App() {
             element={
               <ProtectedRoute>
                 <MarkAttendance />
+              </ProtectedRoute>
+            } 
+          />
+          <Route 
+            path="/student/profile" 
+            element={
+              <ProtectedRoute>
+                <MyProfile />
               </ProtectedRoute>
             } 
           />
